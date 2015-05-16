2b6a645650ec942662b4596212ee06f19b01835f
masnisch <szabo.priszcilla@gmail.com>
2015-05-16 15:48:30 +0200

    My Django Girls app, first commit

---
+++ 0516.py
@@ -0,0 +1,26 @@
+# #print('Hello, Django girls!')
+# if 3<2:
+# 	print('True')
+# elif 3>2:
+# 	print('True')
+# elif 3==3:
+# 	print('djangogirls')
+# else:
+# 	print('False')
+# def hi():
+# 	print ('Hi djangogirls')
+# 	print ('How are you?')
+
+# hi()
+
+# def hi(name):
+# 	print('Hi '+name+'!')
+
+# girls=['Lilla','Bogi','Szilvi','You']
+# for name in girls:
+# 	hi(name)
+# 	print('Next girl')
+
+# for i in range(1,6):
+# 	print(i)
+
